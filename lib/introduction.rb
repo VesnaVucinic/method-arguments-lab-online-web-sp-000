96b5b20149b166e5abc41c462bcc17281a88d511
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-02 12:02:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,4 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name}. I am learning to program in #{language}."
 end
 
-introduction_with_language("Dan", "Ruby")
+introduction_with_language("Dan", "Ember.js")
